7c591add592f2d46fb5473baafaacf863039b285
Patch User <ian.whalen@gmail.com>
2011-07-05 22:13:55 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TopOut::Application.config.secret_token = 'd8263159f72fd57eb7e6fd0b3b55606d0c0ad48ce4126d169ced1cf276e22f89b1c6dda8df90d7c4b666acfa501196c401d03819ce62c82d66fab7e8b7a48585'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+TopOut::Application.config.session_store :cookie_store, :key => '_top_out_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# TopOut::Application.config.session_store :active_record_store
