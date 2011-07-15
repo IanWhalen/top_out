d7f04bc4b363ca3996d577ee6d081b06434b8926
Ian Whalen <ian.whalen@gmail.com>
2011-07-15 11:09:27 -0400

    Added difficulty_position to database for sorting

--- db/migrate/20110706022141_create_problems.rb
+++ db/migrate/20110706022141_create_problems.rb
@@ -4,8 +4,10 @@ class CreateProblems < ActiveRecord::Migration
       t.string :color_one
       t.string :color_two
       t.string :color_three
-      t.string :difficulty
+      t.string :difficulty_display
+      t.integer :difficulty_position
       t.boolean :is_live
+      t.integer :wall_id
 
       t.timestamps
     end
