57f3d2c665ad5e4ccd4e23af94edc5126601ae17
Ian Whalen <ian.whalen@gmail.com>
2011-07-23 13:09:43 -0400

    Redirect to root upon new user creation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -44,7 +44,7 @@ class UsersController < ApplicationController
 
     respond_to do |format|
       if @user.save
-        format.html { redirect_to(@user, :notice => 'Registration successful!') }
+        format.html { redirect_to(root_url, :notice => 'Registration successful!') }
         format.xml  { render :xml => @user, :status => :created, :location => @user }
       else
         format.html { render :action => "new" }
