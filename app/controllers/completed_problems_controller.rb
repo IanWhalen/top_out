35d5ae64b91ebbd9393e7c5c3a1874509db3e26d
Ian Whalen <ian.whalen@gmail.com>
2011-07-24 14:02:41 -0400

    Redirect user to sign_in if attempting to complete problem logged out

--- app/controllers/completed_problems_controller.rb
+++ app/controllers/completed_problems_controller.rb
@@ -1,26 +1,4 @@
 class CompletedProblemsController < ApplicationController
-  # GET /completed_problems
-  # GET /completed_problems.xml
-  def index
-    @completed_problems = CompletedProblem.all
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.xml  { render :xml => @completed_problems }
-    end
-  end
-
-  # GET /completed_problems/1
-  # GET /completed_problems/1.xml
-  def show
-    @completed_problem = CompletedProblem.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.xml  { render :xml => @completed_problem }
-    end
-  end
-
   # GET /completed_problems/new
   # GET /completed_problems/new.xml
   def new
@@ -32,24 +10,18 @@ class CompletedProblemsController < ApplicationController
     end
   end
 
-  # GET /completed_problems/1/edit
-  def edit
-    @completed_problem = CompletedProblem.find(params[:id])
-  end
-
   # POST /completed_problems
   # POST /completed_problems.xml
   def create
-    @completed_problem = CompletedProblem.new(params[:completed_problem])
+    @problem = Problem.find(params[:problem_id])
+    @completed_problem = @problem.completed_problems.build
+    @completed_problem.user = current_user
 
-    respond_to do |format|
-      if @completed_problem.save
-        format.html { redirect_to(@completed_problem, :notice => 'Completed problem was successfully created.') }
-        format.xml  { render :xml => @completed_problem, :status => :created, :location => @completed_problem }
-      else
-        format.html { render :action => "new" }
-        format.xml  { render :xml => @completed_problem.errors, :status => :unprocessable_entity }
-      end
+    if @completed_problem.save
+    elsif @completed_problem.user == nil
+      render :json => { :location => sign_in_path }
+    else
+      render :json => { :location => root_url, :notice => 'No idea what happened there.' }
     end
   end
 
