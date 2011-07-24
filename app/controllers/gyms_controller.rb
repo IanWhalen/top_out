d4f9d4a8af9a6df6fb9d5f30bd0a0cc9e595e9f9
Ian Whalen <ian.whalen@gmail.com>
2011-07-24 14:03:01 -0400

    Removed crufty actions leftover from scaffold

--- app/controllers/gyms_controller.rb
+++ app/controllers/gyms_controller.rb
@@ -37,22 +37,6 @@ class GymsController < ApplicationController
     @gym = Gym.find(params[:id])
   end
 
-  # POST /gyms
-  # POST /gyms.xml
-  def create
-    @gym = Gym.new(params[:gym])
-
-    respond_to do |format|
-      if @gym.save
-        format.html { redirect_to(@gym, :notice => 'Gym was successfully created.') }
-        format.xml  { render :xml => @gym, :status => :created, :location => @gym }
-      else
-        format.html { render :action => "new" }
-        format.xml  { render :xml => @gym.errors, :status => :unprocessable_entity }
-      end
-    end
-  end
-
   # PUT /gyms/1
   # PUT /gyms/1.xml
   def update
