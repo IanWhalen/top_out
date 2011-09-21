5b51414867144e76a4aa9242e1d9dff3c4123469
Ian Whalen <ian.whalen@gmail.com>
2011-09-21 10:40:45 -0400

    Prepping for prod release

--- Gemfile
+++ Gemfile
@@ -1,12 +1,8 @@
 source 'http://rubygems.org'
-gem 'rails', '3.1.0.rc5'
-gem 'sqlite3'
-
-# Use unicorn as the web server
-# gem 'unicorn'
+gem 'rails', '3.1.0'
 
 # Deploy with Capistrano
-# gem 'capistrano'
+gem 'capistrano'
 
 gem 'ruby-debug19', :require => 'ruby-debug'
 
@@ -15,9 +11,18 @@ gem 'hirb'
 gem 'devise'
 gem 'cancan'
 
-# Bundle gems for the local environment. Make sure to
-# put test-only gems in this group so their generators
-# and rake tasks are available in development mode:
+group :production do
+  gem 'mysql'
+end
+
+group :development do
+  gem 'sqlite3'
+end
+
+group :test do
+  gem 'sqlite3'
+end
+
 group :development, :test do
   gem "rspec-rails", ">= 2.6.1"
   gem "factory_girl_rails", ">= 1.1.0"
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -31,6 +31,9 @@ TopOut::Application.configure do
   # In production, Apache or nginx will already do this
   config.serve_static_assets = false
 
+  # Generate digests for assets URLs
+  config.assets.digest = true
+
   # Enable serving of images, stylesheets, and javascripts from an asset server
   # config.action_controller.asset_host = "http://assets.example.com"
 
@@ -46,4 +49,12 @@ TopOut::Application.configure do
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
+
+  config.action_mailer.default_url_options = { :host => 'thetopout.com' }
+  # ActionMailer Config
+  # Setup for production - deliveries, no errors raised
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default :charset => "utf-8"
 end
