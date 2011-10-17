e241d5cca94657bc72f6f32f158d8b873eb4df27
Ian Whalen <ian.whalen@gmail.com>
2011-10-17 00:50:13 -0400

    New colors added as needed

--- app/models/color.rb
+++ app/models/color.rb
@@ -4,6 +4,13 @@ class Color
     'Blue'         => {'hex' => '#0000FF'},
     'Green'        => {'hex' => '#215E21'},
     'Neon Green'   => {'hex' => '#7CFC00'},
-    'Black'        => {'hex' => '#000000'}
+    'Black'        => {'hex' => '#000000'},
+    'Purple'       => {'hex' => '#800080'},
+    'Orange'       => {'hex' => '#FF4500'},
+    'Pink'         => {'hex' => '#FF1493'},
+    'Yellow'       => {'hex' => '#FFFF00'},
+    'Red'          => {'hex' => '#FF0000'},
+    'Silver'       => {'hex' => '#C0C0C0'},
+    'Neon Yellow'  => {'hex' => '#D1FF03'}
   }
 end
