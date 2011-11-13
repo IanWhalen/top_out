5b9bd070b7b9c1ded22260346becd4127127a68d
Ian Whalen <ian.whalen@gmail.com>
2011-11-13 07:27:43 -0500

    Updated message shown on unclimbed problems

--- app/helpers/walls_helper.rb
+++ app/helpers/walls_helper.rb
@@ -1,10 +1,10 @@
 module WallsHelper
   def show_last_completed_problem_for_user(user, problem)
-    return "Not logged in" if current_user.nil?
+    return "" if current_user.nil?
 
     completed = user.last_completed_problem(problem)
 
-    return "Not done yet" if completed.nil?
+    return "" if completed.nil?
  
     return "#{time_ago_in_words(completed.created_at)} ago"
   end
