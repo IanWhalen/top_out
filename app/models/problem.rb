706e8ad807a883df4200bd777d678cfc30c68b37
Ian Whalen <ian.whalen@gmail.com>
2011-11-20 15:45:37 -0500

    Removed old code

--- app/models/problem.rb
+++ app/models/problem.rb
@@ -22,17 +22,6 @@ class Problem < ActiveRecord::Base
     list_colors.to_sentence :last_word_connector => ' & ', :two_words_connector => ' & '
   end
 
-  def last_complete_by_user(user)
-    if not @user = user 
-      @string = "Sign up now!"
-    elsif not @completed_problem = self.completed_problems.select("updated_at").where(:user_id => @user).order("updated_at ASC").last
-      @time = @completed_problem.updated_at
-      @string = time_ago_in_words(@time) + " ago"
-    else
-      @string = "Not Yet"
-    end
-  end
-
   def <=>(other)
     Difficulty.to_int(self.difficulty) <=> Difficulty.to_int(other.difficulty)
   end
