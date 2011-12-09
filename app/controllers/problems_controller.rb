3e5ba1afa7290ef57383633451aa2f927dcc393b
Ian Whalen <ian.whalen@gmail.com>
2011-12-09 15:20:22 -0500

    Removing unused controller actions for now

--- app/controllers/problems_controller.rb
+++ app/controllers/problems_controller.rb
@@ -21,30 +21,4 @@ class ProblemsController < ApplicationController
       render :action => "new"
     end
   end
-
-  # PUT /problems/1
-  def update
-    @problem = Problem.find(params[:id])
-
-    respond_to do |format|
-      if @problem.update_attributes(params[:problem])
-        format.html { redirect_to(@problem, :notice => 'Problem was successfully updated.') }
-        format.xml  { head :ok }
-      else
-        format.html { render :action => "edit" }
-        format.xml  { render :xml => @problem.errors, :status => :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /problems/1
-  def destroy
-    @problem = Problem.find(params[:id])
-    @problem.destroy
-
-    respond_to do |format|
-      format.html { redirect_to(problems_url) }
-      format.xml  { head :ok }
-    end
-  end
 end
