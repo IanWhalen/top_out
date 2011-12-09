9f9f12d12acd06622f956166de41b85ad5c6f75c
Ian Whalen <ian.whalen@gmail.com>
2011-12-09 15:25:34 -0500

    Delete comment

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,6 @@ class User < ActiveRecord::Base
   # Also :token_authenticatable, :confirmable, :encryptable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable
 
-  # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me
 
   has_many :completed_problems
