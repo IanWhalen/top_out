37d369df74035d0e90abbd59bf40dbc9025d946b
Ian Whalen <ian.whalen@gmail.com>
2011-12-09 15:17:26 -0500

    Finally got rid of my only helper method

--- app/helpers/walls_helper.rb
+++ app/helpers/walls_helper.rb
@@ -1,12 +1,2 @@
 module WallsHelper
-  def show_last_completed_problem_for_user(user, problem)
-    return "" if current_user.nil?
-
-    completed = user.last_completed_problem(problem)
-
-    return "" if completed.nil?
- 
-    return "#{time_ago_in_words(completed.created_at)} ago"
-  end
-
 end
