16f6daf24e4de1d3d8708414f00b34462508fc36
Ian Whalen <ian.whalen@gmail.com>
2011-12-10 12:57:45 -0500

    Remove ClimbingSession during rake db:seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,17 +1,15 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
-#   Mayor.create(:name => 'Daley', :city => cities.first)
+# Start with a clean slate
 CompletedProblem.delete_all
-
+ClimbingSession.delete_all
 Gym.delete_all
-@gym = Gym.create(:name => 'Brooklyn Boulders')
-
 Wall.delete_all
 Problem.delete_all
+User.delete_all
+
+# Create test gym
+@gym = Gym.create(:name => 'Brooklyn Boulders')
+
+# Add 2 walls with 3 identical problems each to test gym
 ['The Beast', 'Nor\'Easter (Yellow)'].each do |wall|
   @wall = @gym.walls.create(:name => wall)
   @problem = @wall.problems.create(:color_one => 'Blue', :color_two => 'White', :color_three => 'Green', :difficulty=> "V2+")
@@ -20,7 +18,6 @@ Problem.delete_all
 end
 
 # Create test admin and normal user
-User.delete_all
 @user = User.create(:email => 'user@thetopout.com', :password => 'usertest')
 @admin = User.create(:email => 'admin@thetopout.com', :password => 'admintest')
 @admin.update_attribute :admin, true
