e30ec2270c0aa35dff42162fd8b14c94ce65ed77
Ian Whalen <ian.whalen@gmail.com>
2011-12-16 17:19:33 -0500

    Allow user to post latest ClimbingSession to Facebook

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,6 @@
 class HomeController < ApplicationController
   def index
     @latest_gym = current_user.try(:gym_of_last_completion)
+    @latest_session = current_user.try(:latest_climbing_session)
   end
 end
