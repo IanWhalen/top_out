25b9d4b673f3a3edb783258a0378d4f3266e543e
Ian Whalen <ian.whalen@gmail.com>
2012-01-06 16:51:19 -0500

    Moving JS off of user home page and into application.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,3 +10,14 @@
 $(document).bind("mobileinit", function(){
   $.mobile.linkBindingEnabled = false;
 });
+
+$(function() {
+  $('.session-sparkline').sparkline('html', {
+    type:'bar', barWidth:16, colorMap:["hsl(120,100%,40%)","hsl(113,100%,40%)", "hsl(106,100%,40%)", "hsl(99,100%,40%)", "hsl(92,100%,40%)", "hsl(85,100%,40%)", "hsl(78,100%,40%)", "hsl(71,100%,40%)", "hsl(64,100%,40%)", "hsl(57,100%,40%)", "hsl(50,100%,40%)", "hsl(43,100%,40%)", "hsl(36,100%,40%)", "hsl(29,100%,40%)", "hsl(22,100%,40%)", "hsl(15,100%,40%)", "hsl(8,100%,40%)"]
+    });
+  });
+
+  $('#mainPage').live('pageshow', function() {
+  // Go ahead and draw the now-visible sparkline tags
+  $.sparkline_display_visible();
+});
