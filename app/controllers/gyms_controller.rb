f09db7660bfd0a4fd59e8559db0ac74a9f9bd21d
Ian Whalen <ian.whalen@gmail.com>
2012-01-06 16:18:25 -0500

    Remove code duplication in gym controller using before_filter

--- app/controllers/gyms_controller.rb
+++ app/controllers/gyms_controller.rb
@@ -1,47 +1,16 @@
 class GymsController < ApplicationController
+  before_filter :find_gym, :only => [:show, :edit, :unsolved, :update, :destroy]
+
   def index
     @gyms = Gym.all
-  end
-
-  def show
-    @gym = Gym.find(params[:id])
   end
 
   def new
     @gym = Gym.new
   end
 
-  def edit
-    @gym = Gym.find(params[:id])
-  end
-
-  def unsolved
-    @gym = Gym.find(params[:id])
-  end
-
-  # PUT /gyms/1
-  def update
-    @gym = Gym.find(params[:id])
-
-    respond_to do |format|
-      if @gym.update_attributes(params[:gym])
-        format.html { redirect_to(@gym, :notice => 'Gym was successfully updated.') }
-        format.xml  { head :ok }
-      else
-        format.html { render :action => "edit" }
-        format.xml  { render :xml => @gym.errors, :status => :unprocessable_entity }
-      end
+  protected
+    def find_gym
+      @gym = Gym.find(params[:id])
     end
-  end
-
-  # DELETE /gyms/1
-  def destroy
-    @gym = Gym.find(params[:id])
-    @gym.destroy
-
-    respond_to do |format|
-      format.html { redirect_to(gyms_url) }
-      format.xml  { head :ok }
-    end
-  end
 end
