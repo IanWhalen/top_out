bf05752ef77e97dcb69d240476bcc7eb8ac16d28
Ian Whalen <ian.whalen@gmail.com>
2012-12-25 22:50:28 -0500

    Remove some unused controller actions

--- app/controllers/completed_problems_controller.rb
+++ app/controllers/completed_problems_controller.rb
@@ -1,19 +1,10 @@
 class CompletedProblemsController < ApplicationController
-  # GET /completed_problems/new
-  def new
-    @completed_problem = CompletedProblem.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.xml  { render :xml => @completed_problem }
-    end
-  end
 
   # POST /completed_problems
   def create
     if current_user.nil?
       render :json => { :sign_in_needed => new_user_session_path }
-    else 
+    else
       @problem = Problem.find(params[:problem_id])
       @completed_problem = @problem.completed_problems.build
       @completed_problem.user = current_user
@@ -24,33 +15,7 @@ class CompletedProblemsController < ApplicationController
       else
         render :json => { :location => root_url, :notice => 'No idea what happened there.' }
       end
-  
-    end 
-  end
 
-  # PUT /completed_problems/1
-  def update
-    @completed_problem = CompletedProblem.find(params[:id])
-
-    respond_to do |format|
-      if @completed_problem.update_attributes(params[:completed_problem])
-        format.html { redirect_to(@completed_problem, :notice => 'Completed problem was successfully updated.') }
-        format.xml  { head :ok }
-      else
-        format.html { render :action => "edit" }
-        format.xml  { render :xml => @completed_problem.errors, :status => :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /completed_problems/1
-  def destroy
-    @completed_problem = CompletedProblem.find(params[:id])
-    @completed_problem.destroy
-
-    respond_to do |format|
-      format.html { redirect_to(completed_problems_url) }
-      format.xml  { head :ok }
     end
   end
 end
